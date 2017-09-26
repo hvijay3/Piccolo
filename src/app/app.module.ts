7ebb73e47219734b673ca0f9be8b654393ac7b49
Harshit Vijayvargia <Vijayvargiyah@gmail.com>
2017-09-26 15:23:41 -0400

    Insta Architecture with services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { ImageListComponent } from './image-list/image-list.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavBarComponent,
+    HomeComponent,
+    UserprofileComponent,
+    ImageListComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
