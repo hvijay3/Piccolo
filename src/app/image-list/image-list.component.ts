bc04d871c20d07c9a27da431460056352dbcff1e
19ankits <akki.dos19@gmail.com>
2017-11-30 13:32:21 -0500

    Added feature to display logged in username or search username

--- src/app/image-list/image-list.component.ts
+++ src/app/image-list/image-list.component.ts
@@ -12,6 +12,8 @@ import { ActivatedRoute } from '@angular/router';
 export class ImageListComponent implements OnInit {
   images: Observable<ImageDetails[]>;
   private userId: String;
+  private currentUserName: String;
+  private text: String;
   /* Get logged in user in a variable using home component constructor code
   Set a flag and geeter method for that. Setter method for resettong the flag*/
 
@@ -23,6 +25,18 @@ export class ImageListComponent implements OnInit {
     /* set a flag if current_userid != loggedin userid */
     // this.images = null;
     this.images = this.imageService.getImages(this.userId);           // pass user id here
+    this.isCurrentUserLoggdeInUser();
     console.log('After service call ' + this.userId);
+  }
+
+  isCurrentUserLoggdeInUser() {
+    const loggedInUserId = this.imageService.getUserId();
+    const searchUserId = this.imageService.getCurrentUserId();
+
+    if (loggedInUserId === searchUserId ) {
+      this.currentUserName = 'Welcome ' + this.imageService.getUserName();
+    } else {
+      this.currentUserName = 'ImageList for ' + this.imageService.getSearchUserName();
+    }
   }
 }
